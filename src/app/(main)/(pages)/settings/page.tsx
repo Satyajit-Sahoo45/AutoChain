c954d240f36a48aca91f631c5ffdae115e86749b
Satyajit-Sahoo45 <sahoosatyajit2801@gmail.com>
2024-07-10 20:19:02 +0530

    update tsconfig and added uploadcare

--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -1,9 +1,56 @@
 import ProfileForm from "@/components/forms/profile-form";
 import React from "react";
+import ProfilePicture from "./_components/profile-picture";
+import { db } from "@/lib/db";
 
 type Props = {};
 
 const Settings = (props: Props) => {
+  const authUser = { id: "1" };
+
+  const removeProfileImage = async () => {
+    "use server";
+    const response = await db.user.update({
+      where: {
+        clerkId: authUser.id,
+      },
+      data: {
+        profileImage: "",
+      },
+    });
+    return response;
+  };
+
+  const uploadProfileImage = async (image: string) => {
+    "use server";
+    console.log(image, "878787787887");
+    // const id = authUser.id;
+    // const response = await db.user.update({
+    //   where: {
+    //     clerkId: id,
+    //   },
+    //   data: {
+    //     profileImage: image,
+    //   },
+    // });
+
+    // return response;
+  };
+
+  const updateUserInfo = async (name: string) => {
+    "use server";
+
+    const updateUser = await db.user.update({
+      where: {
+        clerkId: authUser.id,
+      },
+      data: {
+        name,
+      },
+    });
+    return updateUser;
+  };
+
   return (
     <div className="flex flex-col gap-4">
       <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
@@ -16,11 +63,11 @@ const Settings = (props: Props) => {
             Add or update your information
           </p>
         </div>
-        {/* <ProfilePicture
+        <ProfilePicture
           onDelete={removeProfileImage}
-          userImage={user?.profileImage || ""}
+          userImage={""}
           onUpload={uploadProfileImage}
-        /> */}
+        />
         <ProfileForm />
       </div>
     </div>
