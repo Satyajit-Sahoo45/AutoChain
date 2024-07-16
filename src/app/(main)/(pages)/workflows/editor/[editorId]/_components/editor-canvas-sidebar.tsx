981856d19d41ea5aacd2febb2a8489e9a235ee7c
Satyajit-Sahoo45 <sahoosatyajit2801@gmail.com>
2024-07-16 23:31:21 +0530

    added canvas sidebar and onDrag functionalities

---
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/_components/editor-canvas-sidebar.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { CONNECTIONS, EditorCanvasDefaultCardTypes } from "@/lib/constant";
+import { EditorCanvasTypes, EditorNodeType } from "@/lib/types";
+import EditorCanvasIconHelper from "./editor-canvas-card-icon-hepler";
+import { onDragStart } from "@/lib/editor-utils";
+
+type Props = {
+  nodes: EditorNodeType[];
+};
+
+const EditorCanvasSidebar = ({ nodes }: Props) => {
+  return (
+    <aside>
+      <Tabs defaultValue="actions" className="h-screen overflow-scroll pb-24">
+        <TabsList className="bg-transparent">
+          <TabsTrigger value="actions">Actions</TabsTrigger>
+          <TabsTrigger value="settings">Settings</TabsTrigger>
+        </TabsList>
+        <Separator />
+        <TabsContent value="actions" className="flex flex-col gap-4 p-4">
+          {Object.entries(EditorCanvasDefaultCardTypes)
+            .filter(
+              ([_, cardType]) =>
+                (!nodes.length && cardType.type === "Trigger") ||
+                (nodes.length && cardType.type === "Action")
+            )
+            .map(([cardKey, cardValue]) => (
+              <Card
+                key={cardKey}
+                draggable
+                className="w-full cursor-grab border-black bg-neutral-100 dark:border-neutral-700 dark:bg-neutral-900"
+                onDragStart={(event) =>
+                  onDragStart(event, cardKey as EditorCanvasTypes)
+                }
+              >
+                <CardHeader className="flex flex-row items-center gap-4 p-4">
+                  <EditorCanvasIconHelper type={cardKey as EditorCanvasTypes} />
+                  <CardTitle className="text-md">
+                    {cardKey}
+                    <CardDescription>{cardValue.description}</CardDescription>
+                  </CardTitle>
+                </CardHeader>
+              </Card>
+            ))}
+        </TabsContent>
+      </Tabs>
+    </aside>
+  );
+};
+
+export default EditorCanvasSidebar;
